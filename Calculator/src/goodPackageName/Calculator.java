ed1531d81103100e8281e4e7301fce7374739ccc
Ruwaa400 <ruwaa.tareq@gmail.com>
2020-02-22 00:21:42 +0200

    3598458398

--- Calculator/src/goodPackageName/Calculator.java
+++ Calculator/src/goodPackageName/Calculator.java
@@ -7,8 +7,10 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int x, y;
+		
 		System.out.println("first number x:");
 		x = scanner.nextInt();
+
 		System.out.println("second number y:");
 		y = scanner.nextInt();
 
