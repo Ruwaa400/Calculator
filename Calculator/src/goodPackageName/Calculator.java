c6c49a96e62322770519e2f5d5d614e75d7b3fef
Ruwaa400 <ruwaa.tareq@gmail.com>
2020-02-22 01:11:33 +0200

    ant build.xml

--- Calculator/src/goodPackageName/Calculator.java
+++ Calculator/src/goodPackageName/Calculator.java
@@ -7,12 +7,12 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int x, y;
-		
+
 		System.out.println("first number x:");
-		x = scanner.nextInt();
+		x = 10;//scanner.nextInt();
 
 		System.out.println("second number y:");
-		y = scanner.nextInt();
+		y = 10;//scanner.nextInt();
 
 		int z = x + y;
 		System.out.println("x + y = " + z);
