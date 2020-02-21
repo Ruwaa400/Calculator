bd4f123e1901063709080102d83a65049da3efed
Ruwaa400 <ruwaa.tareq@gmail.com>
2020-02-22 00:08:06 +0200

    the code

--- Calculator/src/goodPackageName/Calculator.java
+++ Calculator/src/goodPackageName/Calculator.java
@@ -1,9 +1,20 @@
 package goodPackageName;
 
+import java.util.Scanner;
+
 public class Calculator {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		int x, y;
+		System.out.println("first number x:");
+		x = scanner.nextInt();
+		System.out.println("second number y:");
+		y = scanner.nextInt();
+
+		int z = x + y;
+		System.out.println("x + y = " + z);
+
 	}
 
 }
